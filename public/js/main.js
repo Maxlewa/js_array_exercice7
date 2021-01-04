936395a5d5d288df80ee4dab183ee0561b56b5e8
Maxence <lemairemaxence@hotmail.com>
2021-01-04 10:14:40 +0100

    exo almost ok

---
+++ public/js/main.js
@@ -0,0 +1,27 @@
+// 1
+
+var prenoms = ['louise', 'jamila', 'ayhan', 'maxence'];
+console.log(prenoms);
+
+console.log(prenoms[3]);
+console.log(prenoms.length);
+
+// 2
+
+var monPrenom = ['maxence']
+console.log(monPrenom);
+
+monPrenom.push('alex');
+console.log(monPrenom);
+
+// 3
+
+var classeCoding16 = ['ayhan']
+classeCoding16.push('benedicte');
+classeCoding16.push('louise', 'jamila');
+
+classeCoding16.push('alex');
+classeCoding16.pop('alex');
+
+
+console.log(classeCoding16);
